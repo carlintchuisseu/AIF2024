a7cbc9ae5a3a2aa929e4a4f72f6a6534cb9769d6
carlin <carlintchuisseu@gmail.com>
2023-11-25 23:36:04 +0100

    TP1 completion

--- developpement/MNIST/model.py
+++ developpement/MNIST/model.py
@@ -5,18 +5,18 @@ import torch.nn.functional as F
 class MNISTNet(nn.Module):
     def __init__(self):
         super(MNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 8, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = torch.max_pool2d(x, (2,2))
+        x = self.pool(x)
         x = F.relu(self.conv2(x))
-        x = torch.max_pool2d(x, (2,2))
+        x = self.pool(x)
         x = torch.flatten(x, 1) #or x.view(-1, 16 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
